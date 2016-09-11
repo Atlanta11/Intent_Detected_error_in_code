7dba0ee0724c7a528c611e49d1252cda51458412
Atlanta11 <forever.friends.1980@gmail.com>
2016-09-11 21:08:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/enterprise/ui9/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.enterprise.ui9;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    // broadcast a custom intent.
+    public void broadcastIntent(View view){
+        Intent intent = new Intent();
+        intent.setAction("com.example.enterprise.ui9");
+        sendBroadcast(intent);
+    }
+}
